e39c329a0c3de9a543f997c14b97fc10db0524ee
virgilwjj <virgilwjj@gmail.com>
2021-04-24 20:12:35 +0800

    update

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -48,8 +48,8 @@ std::vector<int> BM(const char s[], const char t[])
     return result;
 }
 
-static const token_index::path_t file_path{"../resource/doc2.txt"};
-static const token_index::path_t query_path{"../resource/query2.txt"};
+static const token_index::path_t file_path{"../resource/depattern_doc.txt"};
+static const token_index::path_t query_path{"../resource/query.txt"};
 
 int main()
 {
@@ -81,23 +81,24 @@ int main()
         for (const auto &token : query)
         {
             token_index::index_set_t index_set = manager.retrieve(token);
-            if (index_set.size() > 5)
+            if (index_set.size() > 1000)
                 continue;
+            //std::cout << token << std::endl;
             for (const auto &index : index_set)
             {
                 token_index::line_t document_line = documents[index]; 
-                auto result = BM(query_line.c_str(), document_line.c_str());
+                auto result = BM(document_line.c_str(), query_line.c_str());
                 if (intersection_set.find(index) != std::end(intersection_set))
+                {
                     std::cout << "yes:" << std::endl;
-                else
-                    std::cout << "no:" << std::endl;
-                std::cout << "  token:" << token << "," << std::endl;
-                std::cout << "  query:" << query_line << "," << std::endl;
-                std::cout << "  document:" << document_line << "," << std::endl;
-                std::cout << "  BM:";
-                for (const auto &num : result)
-                    std::cout << num << ',';
-                std::cout << std::endl; 
+                    std::cout << "  token:" << token << "," << std::endl;
+                    std::cout << "  query:" << query_line << "," << std::endl;
+                    std::cout << "  document:" << document_line << "," << std::endl;
+                    std::cout << "  BM:";
+                    for (const auto &num : result)
+                        std::cout << num << ',';
+                    std::cout << std::endl;
+                }
             }
         }
     }
