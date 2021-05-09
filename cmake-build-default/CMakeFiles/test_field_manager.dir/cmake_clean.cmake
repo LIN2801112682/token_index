9f1a1c70686b526d6bf17e1f7100235bf74cca7c
Qingling <liqinglin_neu@163.com>
2021-05-09 16:20:42 +0800

    update

---
+++ cmake-build-default/CMakeFiles/test_field_manager.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_field_manager.dir/src/bm/bm.cpp.obj"
+  "CMakeFiles/test_field_manager.dir/src/token_index/common.cpp.obj"
+  "CMakeFiles/test_field_manager.dir/src/token_index/field_manager.cpp.obj"
+  "CMakeFiles/test_field_manager.dir/src/token_index/index_manager_v1.cpp.obj"
+  "CMakeFiles/test_field_manager.dir/src/token_index/index_manager_v2.cpp.obj"
+  "CMakeFiles/test_field_manager.dir/src/token_index/index_manager_v3.cpp.obj"
+  "CMakeFiles/test_field_manager.dir/src/token_index/index_manager_v3_1.cpp.obj"
+  "CMakeFiles/test_field_manager.dir/src/token_index/types.cpp.obj"
+  "CMakeFiles/test_field_manager.dir/tests/test_field_manager.cpp.obj"
+  "libtest_field_manager.dll.a"
+  "test_field_manager.exe"
+  "test_field_manager.exe.manifest"
+  "test_field_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_field_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-relwithdebinfo/CMakeFiles/test_bm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for test_bm.
+# This may be replaced when dependencies are built.
