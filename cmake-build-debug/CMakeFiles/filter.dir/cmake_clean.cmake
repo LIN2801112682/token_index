6589828abb755b66ef265d52fda4c606a5f39645
Qingling <liqinglin_neu@163.com>
2021-05-09 15:48:28 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/filter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/filter.dir/tests/filter.cpp.obj"
+  "filter.exe"
+  "filter.exe.manifest"
+  "filter.pdb"
+  "libfilter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
