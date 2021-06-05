63427a9cc713557fb16efe31f41e5c40d6e22a1a
virgilwjj <virgilwjj@gmail.com>
2021-06-05 10:57:24 +0800

    update

--- tests/test_field_regex.cpp
+++ tests/test_field_regex.cpp
@@ -14,6 +14,10 @@ void test_regex(const ti::field_manager &manager, const std::string &field, cons
     {
         auto begin_time = std::chrono::high_resolution_clock::now();
         const auto regex_result = manager.retrieve_field_regex(field, pattern);
+        if (is_out)
+        {
+            os << pattern.flags() << " : " << regex_result << std::endl;
+        }
         auto end_time = std::chrono::high_resolution_clock::now();
         auto elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end_time - begin_time);
         auto program_times = elapsed_time.count();
@@ -32,13 +36,13 @@ void test_regex(const ti::field_manager &manager, const std::string &field, cons
     std::cout << "Average location time: " << avg_time << std::endl;
 }
 
-void test_regex_group(const ti::path_t &field_dir, const std::string &field, const ti::path_t &query_path,
+void test_regex_group(const ti::path_t &field_dir, const std::string &field, const ti::path_t &regex_path,
                 const ti::path_t &regex_result_path)
 {
     ti::field_manager manager{};
     manager.push_field_dir(field_dir);
     // manager.print_field_inverted_index(field);
-    auto regex_vec = ti::load_regex_vec(query_path);
+    auto regex_vec = ti::load_regex_vec(regex_path);
     std::ofstream ofs;
 
     std::cout << "field: " << field << ", regex, cout ----------" << std::endl;
@@ -52,11 +56,11 @@ void test_regex_group(const ti::path_t &field_dir, const std::string &field, con
 
 static const ti::path_t field{"query.txt"};
 static const ti::path_t field_dir{"../resources/field_dir"};
-static const ti::path_t query_path{"../resources/query.txt"};
+static const ti::path_t regex_path{"../resources/regex.txt"};
 static const ti::path_t regex_result_path{"../resources/regex_result.txt"};
 
 int main()
 {
-    test_regex_group(field_dir, field, query_path, regex_result_path);
+    test_regex_group(field_dir, field, regex_path, regex_result_path);
     return 0;
 }
